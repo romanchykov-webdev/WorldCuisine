07d2f01adc549be421fcfd2ec2ae7c7d315223d1
faram <romanchykov.webdev@gmail.com>
2024-11-19 16:58:13 +0100

    create -Add Categories component -Add Recipes component -Add loading component -Add recipe details screen -Finish front end

--- app/RecipeDetailsScreen.jsx
+++ app/RecipeDetailsScreen.jsx
@@ -5,7 +5,7 @@ import {getRecipeDish} from "../api";
 
 import {Image} from 'expo-image'
 import {hp, wp} from "../constants/responsiveScreen";
-import {shadowBoxBlack,  shadowTextSmall} from "../constants/shadow";
+import {shadowBoxBlack, shadowTextSmall} from "../constants/shadow";
 import LoadingComponent from "../components/loadingComponent";
 import {StatusBar} from "expo-status-bar";
 import {LinearGradient} from "expo-linear-gradient";
@@ -14,6 +14,8 @@ import ButtonBack from "../components/ButtonBack";
 import Animated, {FadeInDown, FadeInUp} from "react-native-reanimated";
 import ButtonLike from "../components/ButtonLike";
 import {ClockIcon, FireIcon, Square3Stack3DIcon, UsersIcon} from "react-native-heroicons/mini";
+
+import YouTubeIframe from 'react-native-youtube-iframe'
 
 const RecipeDetailsScreen = () => {
 
@@ -24,6 +26,7 @@ const RecipeDetailsScreen = () => {
 
     const [recipeDish, setRecipeDish] = useState([])
     // console.log('id', id);
+    // console.log('recipeDish', recipeDish);
 
 
     const fetchRecipeDish = async (id) => {
@@ -35,7 +38,7 @@ const RecipeDetailsScreen = () => {
             setRecipeDish(response)
         }
         // setTimeout(() => {
-            setLoading(false)
+        setLoading(false)
         // }, 400)
 
     }
@@ -45,10 +48,40 @@ const RecipeDetailsScreen = () => {
     // console.log('recipeDish', recipeDish);
 
 
+    // get ingredients
+    const ingredientsIndexes = (recipeDish) => {
+        if (!recipeDish) return [];
+
+        let indexes = [];
+        for (let i = 1; i <= 20; i++) {
+            if (recipeDish['strIngredient' + i]) {
+                indexes.push(i)
+            }
+        }
+        // console.log('indexes', indexes)
+        return indexes;
+    }
+    // ingredientsIndexes(recipeDish)
+    // get ingredients
+
+
+    // get video id
+    const getYoutobeVideoId = url => {
+        const regex = /[?&]v=([^&]+)/;
+        const match = url.match(regex);
+
+        if(match && match[1]){
+            return match[1]
+        }
+        return null
+
+    }
+    // get video id
+
     return (
         <ScrollView
             showsVerticalScrollIndicator={false}
-            contentContainerStyle={{paddingBottom: 30, backgroundColor: 'white', flex: 1}}
+            contentContainerStyle={{paddingBottom: 30, backgroundColor: 'white'}}
         >
             <StatusBar style='light'/>
             {
@@ -92,24 +125,28 @@ const RecipeDetailsScreen = () => {
                                     start={{x: 0.5, y: 1}}
                                     end={{x: 0.5, y: 0}}
                                 />
-                                <View className="absolute flex-row justify-between top-[60] pl-5 pr-5  w-full
-                            {/*bg-red-500*/}
-                            ">
+                                <Animated.View
+                                    entering={FadeInUp.duration(400).delay(500)}
+                                    className="absolute flex-row justify-between top-[60] pl-5 pr-5  w-full
+                                    {/*bg-red-500*/}
+                                    ">
                                     <ButtonBack/>
 
                                     <ButtonLike/>
 
-                                </View>
+                                </Animated.View>
                             </View>
                             {/* top image button back and like end*/}
 
                             {/*    dish and description*/}
-                            <View
+                            <Animated.View
+                                entering={FadeInDown.delay(600)}
                                 className="px-4 flex justify-between gap-y-5 "
                             >
                                 {/*    name and area*/}
                                 <View className="gap-y-2">
-                                    <Text style={[{fontSize: hp(2.7)},shadowTextSmall()]} className="font-bold  text-neutral-700">
+                                    <Text style={[{fontSize: hp(2.7)}, shadowTextSmall()]}
+                                          className="font-bold  text-neutral-700">
                                         {recipeDish?.strMeal}
                                     </Text>
                                     <Text style={{fontSize: hp(1.8)}} className="font-medium text-neutral-500">
@@ -117,11 +154,13 @@ const RecipeDetailsScreen = () => {
                                     </Text>
                                 </View>
 
-                            </View>
+                            </Animated.View>
                             {/*    dish and description  end*/}
 
                             {/*    misc     */}
-                            <View className="flex-row justify-around">
+                            <Animated.View
+                                entering={FadeInDown.delay(700)}
+                                className="flex-row justify-around">
 
                                 {/*ClockIcon*/}
                                 <View className="flex rounded-full bg-amber-300 p-2"
@@ -225,19 +264,89 @@ const RecipeDetailsScreen = () => {
 
                                 </View>
 
-                            </View>
+                            </Animated.View>
                             {/*misc end*/}
 
                             {/*    ingredients*/}
-                            <View className="gap-y-4">
+                            <Animated.View
+                                entering={FadeInDown.delay(800)}
+                                className="gap-y-4 px-4">
                                 <Text
-                                    style={[{fontSize: hp(2.5)},shadowTextSmall()]}
+                                    style={[{fontSize: hp(2.5)}, shadowTextSmall()]}
                                     className="font-bold px-4 text-neutral-700"
                                 >
                                     Ingredients
                                 </Text>
-                            </View>
+
+                                {/*    */}
+                                <View className="gap-y-2">
+                                    {
+                                        ingredientsIndexes(recipeDish).map(i => {
+                                            return (
+                                                <View key={i} className="flex-row gap-x-4 items-center">
+                                                    <View style={{height: hp(1.5), width: hp(1.5)}}
+                                                          className="bg-amber-300 rounded-full"
+                                                    />
+                                                    <View className="flex-row gap-x-2">
+                                                        <Text
+                                                            style={{fontSize: hp(1.7)}}
+                                                            className="font-extrabold text-neutral-700">{recipeDish['strIngredient' + i]} -</Text>
+                                                        <Text
+                                                            style={{fontSize: hp(1.7)}}
+                                                            className="font-medium text-neutral-600">{recipeDish['strMeasure' + i]}</Text>
+                                                    </View>
+
+                                                </View>
+                                            )
+                                        })
+                                    }
+                                </View>
+                            </Animated.View>
                             {/*    ingredients  end*/}
+
+                            {/*    instructions*/}
+                            <Animated.View
+                                entering={FadeInDown.delay(800)}
+                                className="gap-y-4 px-4">
+                                <Text
+                                    style={[{fontSize: hp(2.5)}, shadowTextSmall()]}
+                                    className="font-bold px-4 text-neutral-700"
+                                >
+                                    Ingredients
+                                </Text>
+
+                                {/*    */}
+                                <Text style={{fontSize: hp(1.6)}} className="text-neutral-700">
+                                    {recipeDish?.strInstructions}
+                                </Text>
+                            </Animated.View>
+                            {/*    instructions  end*/}
+
+                            {/*    recipe video*/}
+                            {
+                                recipeDish?.strYoutube && (
+                                    <Animated.View
+                                        entering={FadeInDown.delay(900)}
+                                        className="gap-y-4">
+                                        <Text
+                                            style={[{fontSize: hp(2.5)}, shadowTextSmall()]}
+                                            className="font-bold px-4 text-neutral-700"
+                                        >
+                                            Recipe video
+                                        </Text>
+                                        {/*    plaer*/}
+                                        <View className="px-4">
+                                            <YouTubeIframe
+                                                videoId={getYoutobeVideoId(recipeDish?.strYoutube)}
+                                                // videoId='nMyBC9staMU'
+                                                height={hp(30)}
+                                            />
+                                        </View>
+                                    </Animated.View>
+
+                                )
+                            }
+                            {/*    recipe video end*/}
 
                         </View> //end block
 
@@ -251,6 +360,5 @@ const RecipeDetailsScreen = () => {
     );
 };
 
-const styles = StyleSheet.create({})
 
 export default RecipeDetailsScreen;
